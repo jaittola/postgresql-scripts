8b2fa5026b653dba4e93a9dbd7a9bd76b240309f
Jukka Aittola <jaittola@iki.fi>
2025-08-17 19:11:21 +0300

    Remove a table from the schema creation script

--- create_schema.sql
+++ create_schema.sql
@@ -1,10 +1,9 @@
+-- You can add tables and other stuff here if you want
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
-CREATE TABLE IF NOT EXISTS users (
-       id SERIAL PRIMARY KEY,
-       secret_id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
-       username VARCHAR,
-       yachtname VARCHAR,
-       email VARCHAR
-);
+--
+-- CREATE TABLE IF NOT EXISTS users (
+--       id SERIAL PRIMARY KEY,
+--       secret_id UUID NOT NULL UNIQUE DEFAULT uuid_generate_v4(),
+--       username VARCHAR,
+-- );
